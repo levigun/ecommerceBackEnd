e3d0f8a3cc4577b229727f58929a39232a335603
levigun <t.levina31@gmail.com>
2022-03-04 12:37:40 +1100

    created each models and declares their associations

--- models/index.js
+++ models/index.js
@@ -4,13 +4,38 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+// product can only have one category
+Product.hasOne(Category, {
+  foreignKey:'category_id',
+  onDelete: 'CASCADE'
+})
+
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: 'product_tag',
+  as: 'tags',
+  foreignKey: 'product_id'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  // model name of productTag
+  through: 'product_tag',
+  as: 'products',
+  foreignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
