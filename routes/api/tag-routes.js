9b985076db1ee97ac58abe6e1a344941ede1c6bf
levigun <t.levina31@gmail.com>
2022-03-08 10:04:57 +1100

    refactor code and tested routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -33,7 +33,7 @@ router.get('/:id', async (req, res) => {
 });
 
 // create a new tag
-router.post('/',  (req, res) => {
+router.post('/',  async (req, res) => {
   try {
     const tagData = await Tag.create(req.body);
     res.status(200).json(tagData);
@@ -48,14 +48,15 @@ router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   const tagData = await Tag.update(
     { 
-      tag_name: req.body.category_name
+      tag_name: req.body.tag_name
     },
     {
       where: {
         id: req.params.id
       }
     }
-  )
+  );
+  return res.json(tagData);
  
 });
 
